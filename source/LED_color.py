2a9c525d930b237a4a5cb712df0bc0345db478c9
henrikw1 <henrik.woerner@studmail.htw-aalen.de>
2024-06-17 09:22:21 +0200

    änderung 1

---
+++ source/LED_color.py
@@ -0,0 +1,21 @@
+##color.py
+
+class LED_color():
+    def red(self):
+        return (255, 0, 0)
+    def green(self):
+        return (0, 255, 0)
+    def blue(self):
+        return (0, 0, 255)
+    def yellow(self):
+        return (255, 255, 0)
+    def white(self):
+        return(255, 255, 255)
+    def orange(self):
+        return(255, 165, 0)
+    def lightblue(self):
+        return(173, 216, 230)
+    def gold(self):
+        return(255, 215, 0)
+    def magenta(self):
+        return(255, 0, 255)
---
+++ source/led_test_1.py
@@ -0,0 +1,108 @@
+# LED-Streifen
+import time
+from rpi_ws281x import PixelStrip, Color
+import argparse
+import LED_color
+
+class LEDStrip:
+    def __init__(self, led_count, led_pin, led_freq_hz, led_dma, led_brightness, led_invert, led_channel):
+        self.strip = PixelStrip(led_count, led_pin, led_freq_hz, led_dma, led_invert, led_brightness, led_channel)
+        self.strip.begin()
+
+    def color_wipe(self, color, wait_ms=50):
+        """Wipe color across display a pixel at a time."""
+        for i in range(self.strip.numPixels()):
+            self.strip.setPixelColor(i, color)
+            self.strip.show()
+            time.sleep(wait_ms / 1000.0)
+
+    def theater_chase(self, color, wait_ms=50, iterations=10):
+        """Movie theater light style chaser animation."""
+        for j in range(iterations):
+            for q in range(3):
+                for i in range(0, self.strip.numPixels(), 3):
+                    self.strip.setPixelColor(i + q, color)
+                self.strip.show()
+                time.sleep(wait_ms / 1000.0)
+                for i in range(0, self.strip.numPixels(), 3):
+                    self.strip.setPixelColor(i + q, 0)
+
+    def wheel(self, pos):
+        """Generate rainbow colors across 0-255 positions."""
+        if pos < 85:
+            return Color(pos * 3, 255 - pos * 3, 0)
+        elif pos < 170:
+            pos -= 85
+            return Color(255 - pos * 3, 0, pos * 3)
+        else:
+            pos -= 170
+            return Color(0, pos * 3, 255 - pos * 3)
+
+    def rainbow(self, wait_ms=20, iterations=1):
+        """Draw rainbow that fades across all pixels at once."""
+        for j in range(256 * iterations):
+            for i in range(self.strip.numPixels()):
+                self.strip.setPixelColor(i, self.wheel((i + j) & 255))
+            self.strip.show()
+            time.sleep(wait_ms / 1000.0)
+
+    def rainbow_cycle(self, wait_ms=20, iterations=5):
+        """Draw rainbow that uniformly distributes itself across all pixels."""
+        for j in range(256 * iterations):
+            for i in range(self.strip.numPixels()):
+                self.strip.setPixelColor(i, self.wheel(
+                    (int(i * 256 / self.strip.numPixels()) + j) & 255))
+            self.strip.show()
+            time.sleep(wait_ms / 1000.0)
+
+    def theater_chase_rainbow(self, wait_ms=50):
+        """Rainbow movie theater light style chaser animation."""
+        for j in range(256):
+            for q in range(3):
+                for i in range(0, self.strip.numPixels(), 3):
+                    self.strip.setPixelColor(i + q, self.wheel((i + j) % 255))
+                self.strip.show()
+                time.sleep(wait_ms / 1000.0)
+                for i in range(0, self.strip.numPixels(), 3):
+                    self.strip.setPixelColor(i + q, 0)
+
+if __name__ == '__main__':
+    # Process arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+    args = parser.parse_args()
+
+    # LED strip configuration:
+    LED_COUNT = 120        # Number of LED pixels.
+    LED_PIN = 18           # GPIO pin connected to the pixels (18 uses PWM!).
+    LED_FREQ_HZ = 800000   # LED signal frequency in hertz (usually 800khz)
+    LED_DMA = 10           # DMA channel to use for generating signal (try 10)
+    LED_BRIGHTNESS = 255   # Set to 0 for darkest and 255 for brightest
+    LED_INVERT = False     # True to invert the signal (when using NPN transistor level shift)
+    LED_CHANNEL = 0        # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+    # Create LEDStrip object with appropriate configuration.
+    led_strip = LEDStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_BRIGHTNESS, LED_INVERT, LED_CHANNEL)
+
+    print('Press Ctrl-C to quit.')
+    if not args.clear:
+        print('Use "-c" argument to clear LEDs on exit')
+
+    try:
+        while True:
+            print('Color wipe animations.')
+            led_strip.color_wipe(LED_color.red)  # Red wipe
+            led_strip.color_wipe(Color(0, 255, 0))  # Green wipe
+            led_strip.color_wipe(Color(0, 0, 255))  # Blue wipe
+            print('Theater chase animations.')
+            led_strip.theater_chase(Color(127, 127, 127))  # White theater chase
+            led_strip.theater_chase(Color(127, 0, 0))  # Red theater chase
+            led_strip.theater_chase(Color(0, 0, 127))  # Blue theater chase
+            print('Rainbow animations.')
+            led_strip.rainbow()
+            led_strip.rainbow_cycle()
+            led_strip.theater_chase_rainbow()
+
+    except KeyboardInterrupt:
+        if args.clear:
+            led_strip.color_wipe(Color(0, 0, 0), 10)
